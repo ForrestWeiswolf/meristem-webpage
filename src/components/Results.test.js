5b2c6925d65999cad543b41136841bb5b759a433
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-12-09 11:18:37 -0500

    More tests for results component

--- src/components/Results.test.js
+++ src/components/Results.test.js
@@ -2,24 +2,37 @@ import React from 'react'
 import Results from './Results'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
+import Meristem from 'meristem'
 
 describe('Results', () => {
 	let results
 	const str = 'Words, words, words'
 
 	beforeEach(() => {
-		results = shallow(<Results text={str} />)
+		results = shallow(<Results 
+			formatString="'(this)' is a sentence with a subject." 
+			definitions={{'this': 'is a sentence with a subject.'}} 
+		/>)
 	})
 
 	it('renders a div with class "results"', () => {
 		expect(results.hasClass('results')).to.be.true
 	})
 
-	// it('has a \'text\'  prop', () => {
-	// 	expect(results.prop('text')).to.exist
-	// })
+	xit('has a \'formatString\'  prop, which is a string', () => {
+		expect(results.prop('text')).to.exist
+	})
 
-	it('renders that text', () => {
-		expect(results.find('p').text()).to.include(str)
+	xit('has a \'definitions\' prop, which is an object', () => {
+		expect(results.prop('text')).to.exist
+	})
+
+	it('Renders the result of expanding a Meristem format with the formatString and definitions from its props', () => {
+		const format = Meristem.format('\'(this)\' is a sentence with a subject.', {'this': 'is a sentence with a subject.'})
+		expect(results.find('p').text()).to.be(format.expand())
+	})
+
+	xit('rerenders when new props are recieved', () => {
+
 	})
 })

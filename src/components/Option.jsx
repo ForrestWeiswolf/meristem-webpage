c742cb8fbe3583b4a972cd0397a9bf27c4426323
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-04-19 09:56:51 -0400

    Option handles changes

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const Option = props => {
-	return (
-		<div>
-			<input type='text' name='option'/>
-			<input type='number' name='weight'/>
-		</div>
-	)
+class Option extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { option: '', weight: 0 }
+
+		this.handleChange = this.props.handleChange.bind(this)
+	}
+
+	render() {
+		return (
+			<div>
+				<input type='text' name='option'
+					onChange={(e) => {
+						this.setState({ option: e.target.value }, () => {
+							this.handleChange([this.state.option, this.state.weight])
+						})
+					}}
+				/>
+				<input type='number' name='weight'
+					onChange={(e) => {
+						this.setState({ weight: e.target.value }, () => {
+							this.handleChange([this.state.option, this.state.weight])
+						})
+					}}
+				/>
+			</div>
+		)
+	}
 }
 
 Option.propTypes = {
--- src/components/Option.test.js
+++ src/components/Option.test.js
@@ -6,9 +6,10 @@ import Option from './Option'
 
 describe('Option', () => {
 	let option
-	changeSpy = spy()
+	let changeSpy
 
 	beforeEach(() => {
+		changeSpy = spy()
 		option = shallow(< Option handleChange={changeSpy} />)
 	})
 
@@ -16,7 +17,37 @@ describe('Option', () => {
 		expect(option.find('input[type="text"][name="option"]')).to.have.length(1)
 	})
 
+	it('calls its handleChange when "option" is changed', () => {
+		expect(changeSpy.called).to.be.false
+
+		option.find('input[name="option"]').first()
+			.simulate('change', { target: { name: 'option', value: 'foo' } })
+
+		expect(changeSpy.called).to.be.true
+	})
+
 	it('contains a number field named "weight"', () => {
-		expect(option.find('input[type="number"][name="option"]')).to.have.length(1)
+		expect(option.find('input[type="number"][name="weight"]')).to.have.length(1)
+	})
+
+	it('calls its handleChange when "weight" is changed', () => {
+		expect(changeSpy.called).to.be.false
+
+		option.find('input[name="weight"]').first()
+			.simulate('change', { target: { name: 'weight', value: 12 } })
+
+		expect(changeSpy.called).to.be.true
+	})
+
+	it('calls its handleChange with [option, weight]', () => {
+		expect(changeSpy.called).to.be.false
+
+		option.find('input[name="option"]').first()
+			.simulate('change', { target: { name: 'option', value: 'foo' } })
+
+		option.find('input[name="weight"]').first()
+			.simulate('change', { target: { name: 'weight', value: 12 } })
+
+		expect(changeSpy.calledWith(['foo', 12])).to.be.true
 	})
 })
